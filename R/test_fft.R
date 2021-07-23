c2c4f55adccc6b6997c905331dcb9a0e3915f94e
remichel <r.michel@uni-muenster.de>
2021-07-23 12:36:59 +0200

    add circular package and rayleigh test

--- R/test_fft.R
+++ R/test_fft.R
@@ -6,7 +6,9 @@
 #' @param bosc BOSC-Object
 #' @param levels Which levels of data need to be padded? Use "ss" for single subject and "ga" for grand average. Concatenate multiple levels with "-", e.g. "ss-ga"
 #' @param test  Which test to perform?
-#' @param alpha Which alpha level to apply?
+#' @param alpha_amp Which alpha level to apply for amplitude test
+#' @param alpha_complex Which alpha level to apply for complex plane test
+#' @param alpha_phase Which alpha level to apply for rayleigh test
 #' @param overwrite defaults to F
 #'
 #' @return A BOSC-Object
@@ -19,9 +21,9 @@
 #' bosc = simulate_experiment()
 #' bosc = generate_surrogates(bosc)
 #' bosc = fft_bosc(bosc)
-#' bosc = test_fft(bosc, levels = "ga")
+#' bosc = test_fft(bosc, levels = "ga", tests = "amp-complex")
 #'
-test_fft <- function(bosc, levels = "ss-ga", test = "amp", alpha = .05, overwrite = FALSE) {
+test_fft <- function(bosc, levels = "ss-ga", tests = "amp-complex-phase", alpha_amp = .05, alpha_complex = alpha_amp, alpha_phase = alpha_amp, overwrite = FALSE) {
 
   # get levels
   if(!is.character(levels)){
@@ -30,98 +32,133 @@ test_fft <- function(bosc, levels = "ss-ga", test = "amp", alpha = .05, overwrit
     iLevel_list <- split_string_arg(levels, "-")
   }
 
-  # loop through all conditions
+  # get tests
+  if(!is.character(levels)){
+    stop("Argument levels must be a character.")
+  }else{
+    iTest_list <- split_string_arg(tests, "-")
+  }
 
+  message("Starting tests...")
+
+
+  # loop through all conditions
   for (iLevel in iLevel_list) {
 
+    message("\nTest on ", iLevel, ' level...\n')
 
-    message(paste("Test FFT on", iLevel, "..."))
+    for(iTest in iTest_list){
 
-    # check if required data exists
-    if(is.null(bosc$data[[iLevel]]$real$fft) | is.null(bosc$data[[iLevel]]$surrogate$fft)){
-      message(paste("No data found in ", iLevel, ".\nWill continue with next iType/iLevel..."))
-      next
-    }
 
-    # check if test data exists
-    if(!is.null(bosc$test[[iLevel]])){
-      if(overwrite == TRUE){
-        message("Test already exists. Will overwrite...")
-      }else{
-        warning("Test already exists. Will skip to next dataset without performing the FFT...")
+      # check if required data exists
+      if(is.null(bosc$data[[iLevel]]$real$fft) | is.null(bosc$data[[iLevel]]$surrogate$fft)){
+        message(paste("No data found in ", iLevel, ".\nWill continue with next iType/iLevel..."))
         next
       }
-    }
+
+      # check if test data exists
+      if(!is.null(bosc$test[[iLevel]][[iTest]])){
+        if(overwrite == TRUE){
+          message("Test already exists. Will overwrite...")
+        }else{
+          warning("Test already exists. Will skip to next dataset without performing the FFT...")
+          next
+        }
+      }
 
 
 
-    # Perform test
-    if(test == "amp"){
+      # Perform test
+      if(iTest == "amp"){
 
-      # get correct group vars depending on the analysis level
-      if (iLevel == "ss") {
+        message("Comparing Observed FFT Amplitudes against Permutations (alpha = ", alpha_amp,")...")
+
+        # get correct group vars depending on the analysis level
+        if (iLevel == "ss") {
+          group_vars = dplyr::syms(c("subj", "f"))
+        }else{
+          group_vars = dplyr::syms("f")
+        }
+
+        bosc$tests$fft[[iLevel]][[iTest]]$results = bosc$data[[iLevel]]$surrogate$fft %>%
+          dplyr::group_by(.data$n_surr) %>%
+          dplyr::mutate(observed =  !!bosc$data[[iLevel]]$real$fft$amp) %>%
+          dplyr::ungroup() %>%
+          dplyr::group_by(!!!group_vars) %>%
+          dplyr::summarize(crit_value = unname(stats::quantile(.data$amp, probs = 1-!!alpha_amp)),
+                           p = 1-stats::ecdf(.data$amp)(.data$observed),
+                           observed = .data$observed) %>%
+          dplyr::distinct() %>%
+          dplyr::mutate(alpha = !!alpha_amp) %>%
+          dplyr::relocate(.data$alpha, .after = .data$f) %>%
+          dplyr::mutate(sig = dplyr::case_when(.data$observed > .data$crit_value ~ 1,
+                                               .data$observed <= .data$crit_value ~ 0))
+      }else if(iTest == "complex"){
+
         group_vars = dplyr::syms(c("subj", "f"))
-      }else{
-        group_vars = dplyr::syms("f")
+
+        if(iLevel == "ga"){
+          message("Note: Complex vector analysis needs to be performed on single subject data. Will skip and proceed with next test...")
+          next
+        }else if(iLevel == "ss"){
+          message("Compare FFT complex vectors against permutations (alpha = ", alpha_complex,") ...")
+        }
+
+        # save single surrogate data for 2d plot
+        bosc$tests$fft[[iLevel]][[iTest]]$data = bosc$data[[iLevel]]$surrogate$fft %>%
+          dplyr::group_by(.data$n_surr) %>%
+          dplyr::mutate(observed = !!bosc$data[[iLevel]]$real$fft$complex) %>%
+          dplyr::ungroup() %>%
+          dplyr::group_by(.data$n_surr, .data$f) %>%
+          dplyr::summarize(observed = mean(.data$observed),
+                           surrogate = mean(.data$complex)) %>%
+          dplyr::mutate(observed_length = Mod(.data$observed),
+                        surrogate_length = Mod(.data$surrogate),
+                        observed_phase = Arg(.data$observed),
+                        surrogate_phase = Arg(.data$surrogate)) %>%
+          dplyr::ungroup()
+
+        # determine critical vector length for each frequency
+        bosc$tests$fft[[iLevel]][[iTest]]$results <- bosc$tests$fft[[iLevel]][[iTest]]$data %>%
+          dplyr::group_by(.data$f) %>%
+          dplyr::summarize(crit_length = unname(stats::quantile(.data$surrogate_length, probs = 1-!!alpha_complex)),
+                           p = 1-stats::ecdf(.data$surrogate_length)(.data$observed_length),
+                           observed_length = .data$observed_length) %>%
+          dplyr::distinct() %>%
+          dplyr::mutate(alpha = !!alpha_complex) %>%
+          dplyr::relocate(.data$alpha, .after = .data$f) %>%
+          dplyr::mutate(sig = dplyr::case_when(.data$observed_length > .data$crit_length ~ 1,
+                                               .data$observed_length <= .data$crit_length ~ 0))
+
+      }else if(iTest == "phase"){
+
+        # skip grand average level
+        if(iLevel == "ga"){
+          message("Note: Phase analysis needs to be performed on single subject data. Will skip and proceed with next test...")
+          next
+        }
+
+        message("Rayleigh test on phase angles (alpha = ", alpha_phase,")...")
+
+        # rayleigh test
+        bosc$tests$fft[[iLevel]][[iTest]]$results <- bosc$data$ss$real$fft %>%
+          dplyr::group_by(.data$f) %>%
+          dplyr::summarize(mean_phase = circular::mean.circular(circular::circular(.data$phase, units = c("radians"))),
+                           statistic = circular::rayleigh.test(circular::circular(.data$phase, units = c("radians")), mu = NULL)$statistic,
+                           p = circular::rayleigh.test(circular::circular(.data$phase, units = c("radians")), mu = NULL)$p.value,
+                           alpha = !!alpha_phase) %>%
+          dplyr::mutate(sig = case_when(.data$p < .data$alpha ~ 1,
+                                        .data$p >= .data$alpha ~ 0))
+
       }
 
-      bosc$tests$fft$amp[[iLevel]]$results = bosc$data[[iLevel]]$surrogate$fft %>%
-        dplyr::group_by(.data$n_surr) %>%
-        dplyr::mutate(observed =  !!bosc$data[[iLevel]]$real$fft$amp) %>%
-        dplyr::ungroup() %>%
-        dplyr::group_by(!!!group_vars) %>%
-        dplyr::summarize(crit_value = stats::quantile(.data$amp, probs = 1-!!alpha),
-                         p = 1-stats::ecdf(.data$amp)(.data$observed),
-                         observed = .data$observed) %>%
-        dplyr::distinct() %>%
-        dplyr::mutate(alpha = !!alpha) %>%
-        dplyr::relocate(.data$alpha, .after = .data$f) %>%
-        dplyr::mutate(sig = dplyr::case_when(.data$observed > .data$crit_value ~ 1,
-                                             .data$observed <= .data$crit_value ~ 0))
-    }else if(test == "complex"){
-
-      group_vars = dplyr::syms(c("subj", "f"))
-
-      if(iLevel == "ga"){
-        message("This analysis needs to be performed on single subject data. Will skip to next level...")
-        next
-      }else if(iLevel == "ss"){
-        message("Averaging across subjects in complex plane to determine 2D vector lengths...")
-      }
-
-      # save single surrogate data for 2d plot
-      bosc$tests$fft$complex[[iLevel]]$data = bosc$data[[iLevel]]$surrogate$fft %>%
-        dplyr::group_by(.data$n_surr) %>%
-        dplyr::mutate(observed = !!bosc$data[[iLevel]]$real$fft$complex) %>%
-        dplyr::ungroup() %>%
-        dplyr::group_by(.data$n_surr, .data$f) %>%
-        dplyr::summarize(observed = mean(.data$observed),
-                         surrogate = mean(.data$complex)) %>%
-        dplyr::mutate(observed_length = Mod(.data$observed),
-                      surrogate_length = Mod(.data$surrogate),
-                      observed_phase = Arg(.data$observed),
-                      surrogate_phase = Arg(.data$surrogate)) %>%
-        dplyr::ungroup()
-
-      bosc$tests$fft$complex[[iLevel]]$results <- bosc$tests$fft$complex[[iLevel]]$data %>%
-        dplyr::group_by(.data$f) %>%
-        dplyr::summarize(crit_length = stats::quantile(.data$surrogate_length, probs = 1-!!alpha),
-                         p = 1-stats::ecdf(.data$surrogate_length)(.data$observed_length),
-                         observed_length = .data$observed_length) %>%
-        dplyr::distinct() %>%
-        dplyr::mutate(alpha = !!alpha) %>%
-        dplyr::relocate(.data$alpha, .after = .data$f) %>%
-        dplyr::mutate(sig = dplyr::case_when(.data$observed_length > .data$crit_length ~ 1,
-                                             .data$observed_length <= .data$crit_length ~ 0))
 
     }
-
-
   }
 
   # add executed command to history
   bosc$hist <- paste0(bosc$hist, "test_")
 
-  message("Test completed.")
+  message("\nTest completed.")
   return(bosc)
 }
