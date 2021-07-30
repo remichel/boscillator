241ff6652b1b4372462993182c977693cf03d850
remichel <r.michel@uni-muenster.de>
2021-07-30 15:17:57 +0200

    add bosc import fctn

---
+++ R/bosc_import.R
@@ -0,0 +1,71 @@
+#' bosc_import
+#'
+#' @description \code{bosc_import}
+#' Imports a dense sampling study's data frame into boscillator.
+#'
+#' @param data A dataframe with dense sampling data
+#' @param sfreq in Hz
+#' @param vars The index variables in your data frame that identify (1) subjects, (2) timepoints, (3) trials, and (4) the responses. Insert the names as a vector in the order specified before (ie. c = ("subj", "time", "trial", "resp")), regardless of the order they occur in your dataset.
+#' @param level Specify the level of your data, e.g. "single_trial", "ss" or "ga"
+#' @param n_timepoints Number of unique timepoints in dataset
+#' @param aggregate Shall single trial data be averaged to "ss" and "ga"?
+#'
+#' @return bosc-Object
+#' @export bosc_import
+#'
+bosc_import <- function(data, sfreq, n_timepoints, vars = c("subj", "time", "trial", "resp"), level = "single_trial", aggregate = T){
+
+  #create bosc object
+  bosc = bosc()
+
+  # check index variables
+  index_vars = c("subj", "time", "trial", "resp")
+
+  for(iVar in 1:length(vars)){
+    if(vars[iVar] != index_vars[iVar]){
+      message("Warning: Variable ", vars[iVar], ' will be renamed to ', index_vars[iVar])
+    }
+  }
+
+  # subset to relevant variables
+  data <- data %>%
+    dplyr::select(!!vars)
+  # rename variables
+  colnames(data) <- index_vars
+
+  # assign timepoints
+  bosc$timepoints = sort(unique(data$time))
+  bosc$data$single_trial$real$spec$n_timepoints = n_timepoints
+
+  # save sfreq
+  bosc$data$single_trial$real$spec$sfreq = sfreq
+
+  # save data in bosc
+  if(level == "single_trial"){
+    bosc$data$single_trial$real$data = data
+  }else{
+    stop("Your chosen import option is not available yet.")
+  }
+
+  # check if timepoints and sfreq can be reproduced in data
+  if(length(unique(data$time)) !=  n_timepoints){
+    warning("Number of timepoints doesn't match number of timepoints found in dataset")
+  }
+  if(1/(sort(unique(data$time))[2] - sort(unique(data$time))[1]) != sfreq){
+    warning("Sfreq in dataset is ", 1/(sort(unique(data$time))[2] - sort(unique(data$time))[1]), " but ", sfreq, "was piped into sfreq-argument.")
+  }
+
+  message("Found ", length(unique(data$subj)), " subjects.\nFound ", length(unique(data$time)), " timepoints ranging from ", sort(unique(data$time))[1] ," to ", sort(unique(data$time))[n_timepoints] , ".\nFound ", stats::xtabs(~data$subj+data$time)[1], " trials per condition.")
+
+
+  # add executed command to history
+  bosc$hist <- paste0(bosc$hist, "import_")
+
+  # aggregate
+  if(aggregate == T){
+    bosc = aggregate_bosc(bosc)
+  }
+
+  return(bosc)
+
+}
