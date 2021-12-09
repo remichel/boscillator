673fe6d9cfe50462fbc45cf8ce518ff0dfda4621
René Michel <r.michel@wwu.de>
2021-12-09 16:28:08 +0100

    use purrr:map to speed up AR surrogate method

--- R/generate_surrogates.R
+++ R/generate_surrogates.R
@@ -74,22 +74,24 @@ generate_surrogates <-
         }
 
     }else if(method == "ar"){
-      # -------------------------------------------------
-      # still under construction !!!!!!!!!!!!!!!!!!!!!!!
-      # -------------------------------------------------
 
-        # AR1 models for single participants (this might be easier & faster using purrr:map as in simulate_experiment)
-
-      bosc$data$ss$surrogate$data = bosc$data$ss$real$data %>%
-          # repeat dataset n_surr times
-          dplyr::slice(rep(1:dplyr::n(), each = !!n_surr)) %>%
-          # add an identifier variable for each surrogated dataset per subject
-          dplyr::group_by(.data$subj, .data$time) %>%
-          dplyr::mutate(n_surr = dplyr::row_number()) %>%
-          dplyr::ungroup() %>%
-          # for each surrogate dataset, simulate data from an AR1 model
-          dplyr::group_by(.data$subj, .data$n_surr) %>%
-          dplyr::mutate(hr = stats::simulate(forecast::Arima(.data$hr, order = c(1, 0, 0), method = "ML"), nsim = length(!!bosc$timepoints)))
+      bosc$data$ss$surrogate$data <- bosc$data$ss$real$data %>%
+        # fit an AR model to each subject's time course
+        dplyr::group_by(.data$subj) %>%
+        tidyr::nest() %>%
+        dplyr::mutate(model = purrr::map(.data$data, ~ forecast::Arima(y = .x$hr,
+                                                                       order = c(1, 0, 0),
+                                                                       method = "ML"))) %>%
+        # replicate the dataset to generate n_surr rows per subjects
+        dplyr::slice(rep(1:dplyr::n(), each = !!n_surr)) %>%
+        dplyr::group_by(.data$subj) %>%
+        dplyr::mutate(n_surr = dplyr::row_number()) %>%
+        dplyr::ungroup() %>%
+        # simulate a time course from the AR model for each subj and n_surr
+        dplyr::mutate(sim = purrr::map(.data$model, ~ stats::simulate(.x, n_sim = !!bosc$timepoints))) %>%
+        tidyr::unnest(cols = c(data, sim)) %>%
+        dplyr::select(-c(hr, model)) %>%
+        dplyr::rename(hr = sim)
 
       bosc$data$ss$surrogate$spec <- list(
         seed_num = seed_num,
@@ -97,16 +99,20 @@ generate_surrogates <-
         n_surr = n_surr)
 
         # AR1 models from grand average (this might be easier & faster using purrr:map as in simulate_experiment)
-        bosc$data$ga$surrogate$data = bosc$data$ga$real$data %>%
-          # repeat dataset n_surr times
+        bosc$data$ga$surrogate$data <- bosc$data$ga$real$data %>%
+          # fit an AR model to the GA time course
+          tidyr::nest(data = everything()) %>%
+          dplyr::mutate(model = purrr::map(.data$data, ~ forecast::Arima(y = .x$hr,
+                                                                         order = c(1, 0, 0),
+                                                                         method = "ML"))) %>%
+          # replicate the dataset to generate n_surr rows per subjects
           dplyr::slice(rep(1:dplyr::n(), each = !!n_surr)) %>%
-          # add an identifier variable for each surrogated dataset per subject
-          dplyr::group_by(.data$time) %>%
           dplyr::mutate(n_surr = dplyr::row_number()) %>%
-          dplyr::ungroup() %>%
-          # for each surrogate dataset, simulate data from an AR1 model
-          dplyr::group_by(.data$n_surr) %>%
-          dplyr::mutate(hr = stats::simulate(forecast::Arima(.data$hr, order = c(1, 0, 0), method = "ML"), nsim = length(!!bosc$timepoints)))
+          # simulate a time course from the AR model for each n_surr
+          dplyr::mutate(sim = purrr::map(.data$model, ~ stats::simulate(.x, n_sim = !!bosc$timepoints))) %>%
+          tidyr::unnest(cols = c(data, sim)) %>%
+          dplyr::select(-c(hr, model)) %>%
+          dplyr::rename(hr = sim)
 
         # save information about surrogates in a spec file
         bosc$data$ga$surrogate$spec <- list(
