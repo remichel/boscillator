6dd677761a8f303cf2ec91fe8eccbcdf6c531045
remichel <r.michel@uni-muenster.de>
2021-12-15 16:02:11 +0100

    extend high level wrapper by fft and sinmod

--- R/bosc_sim_default.R
+++ R/bosc_sim_default.R
@@ -1,13 +1,17 @@
 #' bosc_sim_default
 #'
 #' @description \code{bosc_sim_default}
-#' Simulates and preprocesses an experiment with default values.
+#' Wrapper function for bosc to simulate, preprocess and statistically test an experiment with default values.
 #'
 #' @param surrogates want to generate surrogates?
 #' @param detrend want to detrend?
 #' @param window want to window?
 #' @param scale want to scale?
 #' @param pad want to pad?
+#' @param fft want to fft?
+#' @param sinmod want sin modelling?
+#' @param fft_test want stat test for fft?
+#' @param sinmod_test want stat test for modelling?
 #'
 #' @return A BOSC-Object
 #' @export bosc_sim_default
@@ -21,26 +25,23 @@ bosc_sim_default <- function(surrogates = T,
                              detrend = T,
                              window = T,
                              scale = F,
-                             pad = F){
+                             pad = F,
+                             fft = F,
+                             sinmod = F,
+                             fft_test = F,
+                             sinmod_test = F){
 
   bosc <- boscillator::simulate_experiment()
 
-  if(surrogates){
-    bosc <- boscillator::generate_surrogates(bosc, n_surr = 10)
-  }
-
-  if(detrend){
-    bosc <- boscillator::detrend_bosc(bosc)
-  }
-  if(window){
-    bosc <- boscillator::window_bosc(bosc)
-  }
-  if(scale){
-    bosc <- scale_bosc(bosc)
-  }
-  if(pad){
-    bosc <- pad_bosc(bosc)
-  }
+  if(surrogates){bosc <- boscillator::generate_surrogates(bosc, n_surr = 10)}
+  if(detrend){bosc <- boscillator::detrend_bosc(bosc)}
+  if(window){bosc <- boscillator::window_bosc(bosc)}
+  if(scale){bosc <- scale_bosc(bosc)}
+  if(pad){bosc <- pad_bosc(bosc)}
+  if(fft){bosc <- fft_bosc(bosc)}
+  if(sinmod){bosc <- sinmod_bosc(bosc)}
+  if(fft_test){bosc <- test_fft(bosc)}
+  if(sinmod_test){bosc <- test_sinmod(bosc)}
 
   return(bosc)
 }
